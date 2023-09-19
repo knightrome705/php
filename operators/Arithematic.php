8bb3b1ae98e7700934f0ba73fe89e58ef7376c75
Martin <knightrome705@gmail.com>
2023-09-19 21:11:52 +0530

    19/09/2023

---
+++ operators/Arithematic.php
@@ -0,0 +1,25 @@
+<?php
+$a=10;
+$b=20;
+echo "<br>".$a;
+echo "<br>".$b;
+echo "<br>";
+echo"Arithematic operator <br>";
+echo"Addition <br>";
+echo $a+$b;
+echo"<br>";
+echo"Substraction<br>";
+echo $b-$a;
+echo"<br>";
+echo"Multiplication<br>";
+echo $a*$b;
+echo"<br>";
+echo"Division<br>";
+echo $b/$a;
+echo"<br>";
+echo"Modolus<br>";
+echo $b%$a;
+echo"<br>";
+echo"Exponenantion<br>";
+echo $b ** $a;
+?>
