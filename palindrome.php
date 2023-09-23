5c0394e16256b0de2881f8041312b9c3dd7c59b8
Martin <knightrome705@gmail.com>
2023-09-23 08:23:33 +0530

    23/09/2023

--- palindrome.php
+++ palindrome.php
@@ -1,7 +1,7 @@
 <?php
 function palinDrome($number){
     echo"Entered number is $number <br>";
-   $value1=strrev((string)$number);
+   $value1=strrev($number);
    echo"Reverse of Entered number is $number <br>";
    if($value1==$number){
     echo"palindrome <br>";
