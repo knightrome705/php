74371a01a6b81c5516e61ac82b727516ad1fe928
Martin <knightrome705@gmail.com>
2023-09-23 08:49:37 +0530

    23/09/2023

--- palindrome.php
+++ palindrome.php
@@ -1,13 +1,15 @@
 <?php
 function palinDrome($number){
+    $number=strtoupper($number);
+    $value2=strtoupper($number);
     echo"Entered number is $number <br>";
-   $value1=strrev($number);
-   echo"Reverse of Entered number is $number <br>";
+   $value1=strrev($value2);
+   echo"Reverse of Entered number is $value1 <br>";
    if($value1==$number){
     echo"palindrome <br>";
    }else{
     echo"Not palindrome <br>";
    }
 }
-palinDrome("153");
+palinDrome("english");
 ?>
---
+++ stringfunctions.php
@@ -0,0 +1,17 @@
+<?php
+
+$a="Martin";
+$b=strlen($a);
+echo"length of the string is $b <br>";
+$c=strpos($a,"artin");
+echo"the occurance of r is $c <br>";
+$d=str_replace("lab","labs","Futura lab");
+echo "String after replace is $d <br>";
+$e=strtoupper($a);
+echo "uppercase $e <br>";
+$f=strtolower($e);
+echo "lowercase $f <br>";
+$d=substr("Futura labs",7,10);
+echo"Substring is $d <br>";
+
+?>
